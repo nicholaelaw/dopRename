a1109c5499d5b776d84756b0d4bad0776f552611
nicholaelaw <nicholaelaw@gmail.com>
2021-05-15 00:40:31 +0800

    thumbnail doesn't work.

--- app.R
+++ app.R
@@ -2,35 +2,45 @@ suppressPackageStartupMessages({
   require(shiny)
   require(shinyjs)
   require(shinyDirectoryInput)
+  require(shinythemes)
   require(DT)
+  require(data.table)
   require(magrittr)
   require(exifr)
+  require(base64enc)
 })
 
-dtLang <- list(url = '//cdn.datatables.net/plug-ins/1.10.20/i18n/Chinese.json')
-options(DT.options=list(autoWidth=F, pageLength=10, language = dtLang))
+dtLang <- list(url = '//cdn.datatables.net/plug-ins/1.10.24/i18n/Chinese.json')
+options(DT.options = list(autoWidth=F, pageLength=10, searching = F))#, language = dtLang
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  theme = shinytheme('darkly'),
+  tags$head(
+    tags$style(HTML("
+      div.DTS tbody tr.even {background-color: inherit !important;}
+      #previewImage img, #thumbnailImage img {max-width: 100%;}
+    "))
+  ),
+
   # Application title
   titlePanel("DOP(DxO) Parsing and Renaming Utility"),
 
-  fluidRow(column(6L, directoryInput('directory', label = 'Select a directory'))),
+  fluidRow(column(6L, directoryInput('directory', label = NULL))),
   fluidRow(
-    column(9L, uiOutput('exifInfoBox'))
+    column(3L, htmlOutput('thumbnailImage')),
+    column(9L, verbatimTextOutput('exifInfoBox'))
   ),
   br(),
   fluidRow(
     column(4L, DTOutput('dopListBox')),
 
-    column(4L, DTOutput('fileListBox')),
+    column(4L, DTOutput('imageListBox'))
 
-    column(4L, htmlOutput('thumbnailImage')),
+    # column(4L, htmlOutput('thumbnailImage')),
   ),
-  fluidRow(
-    column(12L, verbatimTextOutput('directory'))
-  ),
+  fluidRow(column(12L, uiOutput('diagnostics'))),
   fluidRow(column(12L, imageOutput('previewImage')))
 )
 
@@ -39,9 +49,7 @@ server <- function(input, output, session) {
 
   observeEvent(
     ignoreNULL = TRUE,
-    eventExpr = {
-      input$directory
-    },
+    eventExpr = {input$directory},
     handlerExpr = {
       if (input$directory > 0) {
         # condition prevents handler execution on initial app launch
@@ -55,61 +63,112 @@ server <- function(input, output, session) {
     }
   )
 
-  output$directory = renderText({
-    paste0(
-      readDirectoryInput(session, 'directory'),'\n',
-      dopList()[input$dopListBox_rows_selected], '\n',
-      fileList()[input$fileListBox_rows_selected]
+  output$diagnostics <- renderUI({
+
+    placeholderMsg <- 'STANDBY'
+
+    wellPanel(
+      fluidRow(
+        column(6L, verbatimTextOutput('directoryMsg', T))
+      ),
+      fluidRow(
+        column(3L, verbatimTextOutput('dopListSelectedMsg', T)),
+        column(3L, verbatimTextOutput('imageListSelectedMsg', T)),
+        column(3L, verbatimTextOutput('imageListCtMsg', T)),
+        column(3L, verbatimTextOutput('imageExifCtMsg', T))
+      )
+    )
+  })
+  output$directoryMsg <- renderText({paste0(readDirectoryInput(session, 'directory'))})
+  output$dopListSelectedMsg <- renderText({paste0(dopList()[input$dopListBox_rows_selected])})
+  output$imageListSelectedMsg <- renderText({paste0(imageList()[input$imageListBox_rows_selected])})
+  output$imageListCtMsg <- renderText({paste0('Image List: ', imageList()[,.N], ' items')})
+  output$imageExifCtMsg <- renderText({paste0('Image Exif: ', length(imageExif()), ' variables')})
+
+  # fileList ----
+  fileList <- reactive({
+    req(readDirectoryInput(session, 'directory'))
+    data.table(
+      files = dir(readDirectoryInput(session, 'directory'), ignore.case = T)
     )
   })
 
+  # dopList ----
   dopList <- reactive({
-    req(readDirectoryInput(session, 'directory'))
-    dir(readDirectoryInput(session, 'directory'), pattern = '.+\\.dop', ignore.case = T)
+    req(fileList())
+    fileList()[files %like% '\\.dop$', .(`Sidecar Files` = files)]
   })
 
-  fileList <- reactive({
+  # imageList ----
+  imageList <- reactive({
     req(input$dopListBox_rows_selected)
-    selectedDop <- dopList()[input$dopListBox_rows_selected] %>% tools::file_path_sans_ext() %>% paste0(.,'$')
-    dir(readDirectoryInput(session, 'directory'), pattern = selectedDop, ignore.case = T)
+    selected <- dopList()[input$dopListBox_rows_selected] %>% tools::file_path_sans_ext() %>% paste0(.,'$')
+    fileList()[files %like% selected & !(files %like% '\\.dop$'), .(`Image Files` = files)]
   })
 
-  output$dopListBox <- renderDT({
-    # req(readDirectoryInput(session, 'directory'))
-    # path <- readDirectoryInput(session, 'directory')
-    # dopList <- dir(path, pattern = '.+\\.dop', ignore.case = T)
-    datatable(data.frame('dop files' = dopList()), selection = 'single', style = 'bootstrap')
+  # imageExif ----
+  imageExif <- reactive({
+    req(imageList()[,.N > 0L])
+    req(input$imageListBox_rows_selected)
+    paste0(readDirectoryInput(session, 'directory'), imageList()[input$imageListBox_rows_selected]) %>% read_exif()
   })
 
-  output$fileListBox <- renderDT({
-    # req(input$dopListBox_rows_selected)
-    datatable(data.frame('matches' = fileList()), selection = list(mode = 'single', selected = 1L), style = 'bootstrap')
-  })
-
-
-
-
-
-  # x <- read_exif('example/20181005150239-_MGL0212.CR2')
-  # output$previewImage <- renderImage({list(src = paste0('data:image/jpeg;', gsub('base64:', 'base64,', x$PreviewImage)))}, deleteFile = T)
-  # output$previewImage <- renderImage({list(src = paste0('data:image/jpeg;', gsub('base64:', 'base64,', x$ThumbnailImage)))}, deleteFile = T)
-
-  # output$thumbnailImage <- renderUI({
-  #   req(input$fileListBox_rows_selected)
-  #   x <-paste0(readDirectoryInput(session, 'directory'), fileList()[input$fileListBox_rows_selected]) %>% read_exif()
-  #   tags$img(src = paste0('data:image/jpeg;', gsub('base64:', 'base64,', x$ThumbnailImage)))
+  # output$exifInfoBox <- renderText({
+  #   req(imageList()[,.N > 0L])
+  #   req(input$imageListBox_rows_selected)
+  #   x <- imageExif()
+  #   paste0(
+  #     x$CreateDate, '\n',
+  #     x$Model, '\n'
+  #   )
   # })
 
-  observe({
-    req(input$fileListBox_rows_selected)
-    x <-paste0(readDirectoryInput(session, 'directory'), fileList()[input$fileListBox_rows_selected]) %>% read_exif()
 
-    output$thumbnailImage <- renderUI({tags$img(src = paste0('data:image/jpeg;', gsub('base64:', 'base64,', x$ThumbnailImage)))})
-    output$previewImage <- renderImage({
-      tempImg <- tempfile(pattern = 'preview_', fileext = '.jpg')
-      writeBin(x$PreviewImage %>% base64enc::base64decode(), tempImg)
-      list(src = tempImg, contentType = 'image/jpeg')
+  output$dopListBox <- renderDT({
+    req(fileList())
+    datatable(
+      dopList(),# fileList()[files %like% '\\.dop$', .(`Sidecar files` = files)],
+      selection = 'single', style = 'bootstrap', extensions = 'Scroller',
+      options = list(scrollY = 370, scroller = list(loadingIndicator = T))
+    )
+  })
+
+  output$imageListBox <- renderDT({
+    req(input$dopListBox_rows_selected)
+    datatable(
+      imageList(),
+      selection = list(mode = 'single', selected = 1L), style = 'bootstrap',
+      options = list(dom = 'ti')
+    )
+  })
+
+
+
+
+
+
+  observe({
+    req(imageList()[,.N > 0L])
+    req(input$imageListBox_rows_selected)
+    x <- imageExif()
+
+    tempThumb <- tempfile(pattern = 'thumb_', fileext = '.jpg')
+    con <- file(tempThumb, 'wb')
+    x$ThumbnailImage %>% substr(., 8, nchar(.)) %>% base64decode(., con)
+    close(con)
+
+    # tempPreview <- tempfile(pattern = 'preview_', fileext = '.jpg')
+    # con <- file(tempPreview, 'wb')
+    # x$PreviewImage %>% substr(., 8, nchar(.)) %>% base64decode(., con)
+    # close(con)
+
+    output$thumbnailImage <- renderImage({
+      list(src = tempThumb, contentType = 'image/jpeg')
     }, deleteFile = T)
+
+    # output$previewImage <- renderImage({
+    #   list(src = tempPreview, contentType = 'image/jpeg')
+    # }, deleteFile = T)
   })
 }
 
---
+++ purgatory.R
@@ -0,0 +1,40 @@
+x <- data.table(
+  `Sidecar files` = dir('example/', pattern = '.+\\.dop', ignore.case = T)
+)
+
+y <- data.table(
+  Matches = dir('example/', ignore.case = T)
+)
+
+x <-paste0('example/', '20181005123724-_MGL0123.CR2') %>% read_exif()
+tempImg <- tempfile(pattern = 'preview_', fileext = '.jpg')
+writeBin(base64enc::base64decode(x$PreviewImage), tempImg)
+base64enc::base64decode(x$ThumbnailImage, file(tempImg,'wb'))
+
+paste0('data:image/jpeg;', gsub('base64:', 'base64,', x$ThumbnailImage)) %>%
+  base64enc::base64decode(., file(tempImg,'wb'))
+
+paste0('data:image/jpeg;', gsub('base64:', 'base64,', x$ThumbnailImage)) %>%
+  strsplit(.,",")
+
+x$PreviewImage %>% substr(., 8, nchar(.)) %>% base64enc::base64decode(., file('example/test.jpg', 'wb'))
+
+# fiddling around to make absolutely sure on how to dump images from exif bundle.
+path <- 'example/20181003150115-_MGL8642-2-389.CR2';path2 <- substr(path,1L,nchar(path)-4L)
+x <- path %>% read_exif(tags = c('ThumbnailImage','PreviewImage'))
+con <- file(paste0(path2,'_preview_64.jpg'), 'wb'); x$PreviewImage %>% substr(., 8, nchar(.)) %>% base64enc::base64decode(., con); close(con)
+con <- file(paste0(path2,'_thumb_64.jpg'), 'wb'); x$ThumbnailImage %>% substr(., 8, nchar(.)) %>% base64enc::base64decode(., con); close(con)
+exiftool_call(args = '-b -PreviewImage -w _preview.jpg', fnames = path)
+exiftool_call(args = '-b -ThumbnailImage -w _thumb.jpg', fnames = path)
+
+p1 <- x$PreviewImage %>% substr(., 8, nchar(.))
+p2 <- base64encode(paste0(path2, '_preview_64.jpg'))
+p3 <- base64encode(paste0(path2, '_preview.jpg'))
+
+
+t1 <- x$ThumbnailImage %>% substr(., 8, nchar(.))
+t2 <- base64encode(paste0(path2, '_thumb_64.jpg'))
+t3 <- base64encode(paste0(path2, '_thumb.jpg'))
+
+nchar(c(p1, p2, p3))
+nchar(c(t1, t2, t3))
